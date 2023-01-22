eac8f798102cd236f350261a43beef9a12d6b833
gsdutra <gabrielsprecherdutra@gmail.com>
2023-01-22 20:25:13 -0300

    feat: created controlles and routers directories

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from 'cors';
-import router from './routers/index.js'
+import router from './routes/index.js'
 
 const app = express();
 app.use(express.json());
