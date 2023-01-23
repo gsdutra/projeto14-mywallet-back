c929e400b3584e9a11294e7b66ea6622c602aa91
gsdutra <gabrielsprecherdutra@gmail.com>
2023-01-23 16:52:26 -0300

    feat: total balance added to get transactions

--- src/controllers/getTransactions.js
+++ src/controllers/getTransactions.js
@@ -6,5 +6,25 @@ export async function getTransactions(req, res){
 	const userId = res.locals.userId
 	const transactions = await db.collection("transactions").find({userId: ObjectID(userId)}).toArray()
 
-	res.status(200).send(transactions)
+	let balance = 0.0;
+	let balanceType = '';
+
+	for (let i = 0; i < transactions.length; i++) {
+		if (transactions[i].type==='received'){
+			balance+=Number(transactions[i].ammount)
+		}else{
+			balance-=Number(transactions[i].ammount)
+		}	
+	}
+
+	if (balance < 0){
+		balance = balance * (-1)
+		balanceType = 'spent'
+	}else{
+		balanceType = 'received'
+	}
+
+	balance = balance.toFixed(2)
+
+	res.status(200).send({transactions, balance, balanceType})
 }
